88d7c8d45a0b24b25036aa48cab0ce5866213670
Arriziq <114041758+Arriziqrzyx@users.noreply.github.com>
2024-06-17 02:54:48 +0700

    Screen Switching

--- App.jsx
+++ App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+      <Button
+        title="Pergi ke Progate"
+        onPress={() => navigation.navigate('Progate')}
+      />
+    </View>
+  )
+}
+
+const ProgateScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Progate!</Text>
+      <Button title="Kembali" onPress={() => navigation.goBack()} />
     </View>
   )
 }
@@ -16,8 +29,9 @@ const Stack = createNativeStackNavigator()
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Progate" component={ProgateScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
